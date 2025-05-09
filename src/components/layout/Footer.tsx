474af7bfee5f496777971c5878ed93d60a0af84c
Jordan Lambrecht <me@jordanlambrecht.com>
2025-05-09 11:28:59 -0500

    fix: update footer styles for better responsiveness and remove italic from text

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,9 +2,9 @@
 
 export function Footer() {
   return (
-    <footer className="block w-full px-4 py-12 mt-24 ">
+    <footer className="block w-full px-4 py-12 mt-24 md:max-w-7xl mx-auto">
       <div className="container flex flex-col items-start justify-center gap-y-4">
-        <p className="text-sm italic text-black">Thanks for hangin&apos;</p>
+        <p className="text-sm text-black">Thanks for hangin&apos;</p>
         <p className="text-sm text-black">© {new Date().getFullYear()}</p>
       </div>
     </footer>
